1f287e96d6b1547b09345a8e3f0852f508bd104f
Kayvon Ranjbar <kayvon.ranjbar@yahoo.com>
2020-09-26 20:57:02 -0600

    Created an extension method

---
+++ Features/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Features.Linq;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott" },
+                new Employee { Id = 2, Name = "Chris" }
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>
+            {
+                new Employee { Id = 3, Name = "Alex" }
+            };
+
+            Console.WriteLine(developers.Count());
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+        }
+    }
+}
