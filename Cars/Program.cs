32523713170762f649b1d8c6ee78b8969866666c
Kayvon Ranjbar <kayvon.ranjbar@yahoo.com>
2020-09-30 22:01:35 -0600

    Implemented a file processor

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace Cars
     {
         static void Main(string[] args)
         {
+            var cars = ProcessFile("fuel.csv");
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            var query =
+                from line in File.ReadAllLines(path).Skip(1)
+                where line.Length > 1
+                select Car.ParseFromCsv(line);
+
+            return query.ToList();
         }
     }
 }
