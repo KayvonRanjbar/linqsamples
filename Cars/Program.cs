d9f2c87697c9fbeabb2c3c2da23f1dc34b3ac8c9
Kayvon Ranjbar <kayvon.ranjbar@yahoo.com>
2020-10-01 20:40:51 -0600

    Projected data with select

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -17,11 +17,15 @@ namespace Cars
                 from car in cars
                 where car.Manufacturer == "BMW" && car.Year == 2016
                 orderby car.Combined descending, car.Name ascending
-                select car;
+                select new
+                {
+                    car.Manufacturer,
+                    car.Name,
+                    car.Combined
+                };
 
-            var result = cars.All(c => c.Manufacturer == "Ford");
-
-            Console.WriteLine(result);
+            var result = cars.Select(c => new { c.Manufacturer,
+                                                c.Name, c.Combined });
 
             foreach (var car in query.Take(10))
             {
@@ -32,11 +36,35 @@ namespace Cars
         private static List<Car> ProcessFile(string path)
         {
             var query =
-                from line in File.ReadAllLines(path).Skip(1)
-                where line.Length > 1
-                select Car.ParseFromCsv(line);
+
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(l => l.Length > 1)
+                    .ToCar();
 
             return query.ToList();
+        }
+    }
+
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                }; 
+            }
         }
     }
 }
